68e05b999f2dd6f3b7e648d5f2c2d809e9d0cf93
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-01-25 03:10:12 +0600

    User can visit service by category and if category is emty then show some text

--- Client-Side/src/components/Rooms/Rooms.jsx
+++ Client-Side/src/components/Rooms/Rooms.jsx
@@ -1,23 +1,41 @@
 import { useEffect, useState } from "react";
 import Cards from "./Cards";
 import Container from "../Shared/Container";
+import { useSearchParams } from "react-router-dom";
+import Heading from "../Shared/Heading";
 
 const Rooms = () => {
   const [rooms, setRooms] = useState([]);
+  const [params, setParams] = useSearchParams();
+  const category = params.get("category");
+
   useEffect(() => {
     fetch("/rooms.json")
       .then((res) => res.json())
       .then((data) => {
-        setRooms(data);
+        if (category) {
+          const filtered = data.filter((room) => room.category === category);
+          setRooms(filtered);
+        } else setRooms(data);
       });
-  }, []);
+  }, [category]);
   return (
     <Container>
-      <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {rooms?.map((room) => (
-          <Cards key={room?._id} room={room} />
-        ))}
-      </div>
+      {rooms && rooms.length > 0 ? (
+        <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {rooms?.map((room) => (
+            <Cards key={room?._id} room={room} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex justify-center items-center min-h-[calc(100vh-300px)]">
+          <Heading
+            center={true}
+            title={"No Rooms Available In this Category!"}
+            subtitle={"Please Select Others Categories."}
+          />
+        </div>
+      )}
     </Container>
   );
 };
---
+++ Client-Side/src/components/Shared/Heading.jsx
@@ -0,0 +1,10 @@
+const Heading = ({center,title,subtitle}) => {
+  return (
+    <div className={center ? "text-center" : "text-start"}>
+      <div className="text-2xl font-bold">{title}</div>
+      <div className="font-light text-neutral-500 mt-2">{subtitle}</div>
+    </div>
+  );
+};
+
+export default Heading;
