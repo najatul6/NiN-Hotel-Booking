d1a1315085eb2b373fe6d7939f7361dd2647fcc8
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-01-29 22:09:34 +0600

    get all data from form when host submit

--- Client-Side/src/pages/Dashboard/Host/AddRoom/AddRoom.jsx
+++ Client-Side/src/pages/Dashboard/Host/AddRoom/AddRoom.jsx
@@ -1,14 +1,17 @@
 import { Helmet } from "react-helmet-async";
 import AddRoomForm from "../../../../components/Form/AddRoomFrom";
 import { useState } from "react";
+import { imageUpload } from "../../../../Utils/ImageUpload";
+import useAuth from "../../../../hooks/useAuth";
 
 const AddRoom = () => {
+  const { user } = useAuth();
   const [dates, setDates] = useState({
     startDate: new Date(),
     endDate: new Date(),
     key: "selection",
   });
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const form = event.target;
     const location = form.location.value;
@@ -18,17 +21,36 @@ const AddRoom = () => {
     const from = dates?.startDate;
     const image = form.image.files[0];
     const price = form.price.value;
-    const total_guest = form.total_guest.value;
+    const guests = form.total_guest.value;
     const bedrooms = form.bedrooms.value;
     const bathrooms = form.bathrooms.value;
     const description = form.description.value;
-
-    console.log("hello");
+    const host = {
+      name: user?.displayName,
+      image: user?.photoURL,
+      email: user?.email,
+    };
+    const image_url = await imageUpload(image);
+    const roomData = {
+      location,
+      title,
+      from,
+      to,
+      price,
+      guests,
+      bedrooms,
+      bathrooms,
+      description,
+      image: image_url?.display_url,
+      host,
+      category,
+    };
+    console.log("hello", roomData);
   };
 
   //   Handle date change from react-date-range calender
   const handleDates = (ranges) => {
-    console.log(ranges)
+    console.log(ranges);
     setDates(ranges?.selection);
   };
   return (
@@ -37,7 +59,11 @@ const AddRoom = () => {
         <title>Add Room | Dashboard</title>
       </Helmet>
       {/* Room From */}
-      <AddRoomForm handleSubmit={handleSubmit} handleDates={handleDates} dates={dates} />
+      <AddRoomForm
+        handleSubmit={handleSubmit}
+        handleDates={handleDates}
+        dates={dates}
+      />
     </div>
   );
 };
