7319e3157a01c8261baab6709d5e11ecd5650550
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-01-29 19:38:29 +0600

    Handle date change from react-date-range calender

--- Client-Side/src/pages/Dashboard/Host/AddRoom/AddRoom.jsx
+++ Client-Side/src/pages/Dashboard/Host/AddRoom/AddRoom.jsx
@@ -1,16 +1,22 @@
 import { Helmet } from "react-helmet-async";
 import AddRoomForm from "../../../../components/Form/AddRoomFrom";
+import { useState } from "react";
 
 const AddRoom = () => {
+  const [dates, setDates] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
     const location = form.location.value;
     const category = form.category.value;
     const title = form.title.value;
-    const to = "";
-    const from = "";
-    const image = form.image.value;
+    const to = dates?.endDate;
+    const from = dates?.startDate;
+    const image = form.image.files[0];
     const price = form.price.value;
     const total_guest = form.total_guest.value;
     const bedrooms = form.bedrooms.value;
@@ -19,13 +25,18 @@ const AddRoom = () => {
 
     console.log("hello");
   };
+
+  //   Handle date change from react-date-range calender
+  const handleDates = (ranges) => {
+    setDates(ranges?.selection);
+  };
   return (
     <div>
       <Helmet>
         <title>Add Room | Dashboard</title>
       </Helmet>
       {/* Room From */}
-      <AddRoomForm handleSubmit={handleSubmit} />
+      <AddRoomForm handleSubmit={handleSubmit} handleDates={handleDates} dates={dates} />
     </div>
   );
 };
