b20e48ab0c07201c18fee5adbb0c59c3b4a81875
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-01-29 19:28:44 +0600

    Get all data from form

--- Client-Side/src/components/Form/AddRoomFrom.jsx
+++ Client-Side/src/components/Form/AddRoomFrom.jsx
@@ -11,7 +11,7 @@ const AddRoomForm = ({
 }) => {
   return (
     <div className="w-full min-h-[calc(100vh-40px)] flex flex-col justify-center items-center text-gray-800 rounded-xl bg-gray-50">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
           <div className="space-y-6">
             <div className="space-y-1 text-sm">
@@ -49,7 +49,7 @@ const AddRoomForm = ({
               <label htmlFor="location" className="block text-gray-600">
                 Select Availability Range
               </label>
-              <DateRange rangeColors={["#F43F5E"]} />
+              <DateRange rangeColors={["#ff6d01"]} />
             </div>
           </div>
           <div className="space-y-6">
--- Client-Side/src/pages/Dashboard/Host/AddRoom/AddRoom.jsx
+++ Client-Side/src/pages/Dashboard/Host/AddRoom/AddRoom.jsx
@@ -1,7 +1,33 @@
+import { Helmet } from "react-helmet-async";
 import AddRoomForm from "../../../../components/Form/AddRoomFrom";
 
-const Addroom = () => {
-  return <AddRoomForm />;
+const AddRoom = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const location = form.location.value;
+    const category = form.category.value;
+    const title = form.title.value;
+    const to = "";
+    const from = "";
+    const image = form.image.value;
+    const price = form.price.value;
+    const total_guest = form.total_guest.value;
+    const bedrooms = form.bedrooms.value;
+    const bathrooms = form.bathrooms.value;
+    const description = form.description.value;
+
+    console.log("hello");
+  };
+  return (
+    <div>
+      <Helmet>
+        <title>Add Room | Dashboard</title>
+      </Helmet>
+      {/* Room From */}
+      <AddRoomForm handleSubmit={handleSubmit} />
+    </div>
+  );
 };
 
-export default Addroom;
+export default AddRoom;
