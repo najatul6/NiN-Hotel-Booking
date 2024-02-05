5ce82f65ecb06ae03ae779f5265ba7b8b595607e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-05 19:11:22 +0600

    Create Table heading location

--- Client-Side/src/pages/Dashboard/Host/MyListings/MyListings.jsx
+++ Client-Side/src/pages/Dashboard/Host/MyListings/MyListings.jsx
@@ -34,6 +34,12 @@ const MyListings = () => {
                       scope="col"
                       className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
                     >
+                      Location
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                    >
                       Price
                     </th>
                     <th
