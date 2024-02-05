be4e20d824a80b590c478e16b2619efc65c59571
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-05 19:08:27 +0600

    Create a function for host role

--- Client-Side/src/routes/HostRoute.jsx
+++ Client-Side/src/routes/HostRoute.jsx
@@ -1,3 +1,9 @@
 import { Navigate } from "react-router-dom";
 import Loader from "../components/Shared/Loader";
 import useRole from "../hooks/useRole";
+const HostRoute = ({ children }) => {
+  const [role, isLoading] = useRole();
+  if (isLoading) return <Loader />;
+  if (role === "host") return children;
+  return <Navigate to="/dashboard" />;
+};
