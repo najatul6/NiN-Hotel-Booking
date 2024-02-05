52ec69788ef2e62ba542a40b64ee5c4ca4006715
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-05 19:05:49 +0600

    Create host route

--- Client-Side/src/hooks/useRole.jsx
+++ Client-Side/src/hooks/useRole.jsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from "react";
 import useAuth from "./useAuth";
 import { getRole } from "../Utils/auth";
 import { useQuery } from "@tanstack/react-query";
 
 const useRole = () => {
   const { user,loading } = useAuth();
-  // const [role, setRole] = useState(null);
-  // const [loading,setLoading]=useState(true)
-  // useEffect(() => {
-  //   setLoading(true)
-  //   getRole(user?.email).then(data => {
-  //     setRole(data);
-  //     setLoading(false)
-  //   });
-  // }, [user]);
   const {data:role,isLoading}=useQuery({
     enabled: !loading && !!user?.email,
     queryFn:async()=>await getRole(user?.email),
