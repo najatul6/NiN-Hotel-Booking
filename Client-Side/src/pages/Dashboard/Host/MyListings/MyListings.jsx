8d35cb5206039475458d9901829c58a11e0d019c
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-05 19:10:13 +0600

    update talble

--- Client-Side/src/pages/Dashboard/Host/MyListings/MyListings.jsx
+++ Client-Side/src/pages/Dashboard/Host/MyListings/MyListings.jsx
@@ -24,18 +24,7 @@ const MyListings = () => {
               <table className="min-w-full leading-normal">
                 <thead>
                   <tr>
-                    <th
-                      scope="col"
-                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
-                    >
-                      Title
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
-                    >
-                      Location
-                    </th>
+                    
                     <th
                       scope="col"
                       className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
--- Client-Side/src/routes/HostRoute.jsx
+++ Client-Side/src/routes/HostRoute.jsx
@@ -1,6 +1,7 @@
 import { Navigate } from "react-router-dom";
 import Loader from "../components/Shared/Loader";
 import useRole from "../hooks/useRole";
+
 const HostRoute = ({ children }) => {
   const [role, isLoading] = useRole();
   if (isLoading) return <Loader />;
