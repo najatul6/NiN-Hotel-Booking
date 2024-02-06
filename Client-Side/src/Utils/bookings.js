df357ec5bcc5fb66a4dd2fc8a0fb7bbb9cf83081
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-06 23:25:39 +0600

    create a collection for payment

--- Client-Side/src/Utils/bookings.js
+++ Client-Side/src/Utils/bookings.js
@@ -1,4 +1,5 @@
 import axiosSecure from "./axiosSecure"; 
 export const createPaymentIntent=async(price)=>{
-    const {data}=await
+    const {data}=await axiosSecure.post('/create-payment-intent',price)
+    return data
 }
