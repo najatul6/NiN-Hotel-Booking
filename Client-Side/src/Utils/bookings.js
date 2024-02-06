f25581457eaa6f326fe69fa88d3de7af2ff5058e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-06 23:56:44 +0600

    Client can resarve a service and pay for it

--- Client-Side/src/Utils/bookings.js
+++ Client-Side/src/Utils/bookings.js
@@ -1,5 +1,7 @@
-import axiosSecure from "./axiosSecure"; 
-export const createPaymentIntent=async(price)=>{
-    const {data}=await axiosSecure.post('/create-payment-intent',price)
-    return data
-}
+import axiosSecure from "./axiosSecure";
+
+// Create Payment intent
+export const createPaymentIntent = async (price) => {
+  const { data } = await axiosSecure.post("/create-payment-intent", price);
+  return data;
+};
