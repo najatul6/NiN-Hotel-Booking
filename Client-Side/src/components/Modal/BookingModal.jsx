a5052eec8a3348a85a40f6123e64a4e562d26e5a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-07 23:28:19 +0600

    create modal

--- Client-Side/src/components/Modal/BookingModal.jsx
+++ Client-Side/src/components/Modal/BookingModal.jsx
@@ -52,11 +52,7 @@ const BookingModal = ({ closeModal, isOpen, bookingInfo }) => {
                     Location: {bookingInfo.location}
                   </p>
                 </div>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500">
-                    Guest: {bookingInfo.guest.name}
-                  </p>
-                </div>
+                
                 <div className="mt-2">
                   <p className="text-sm text-gray-500">
                     From: {format(new Date(bookingInfo.from), "PP")} - To:{" "}
