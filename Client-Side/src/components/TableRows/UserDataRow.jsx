518d64f69645ac06eae2de725d9664aae076748c
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-09 21:44:36 +0600

    show users data and create new component user data row

--- Client-Side/src/components/TableRows/UserDataRow.jsx
+++ Client-Side/src/components/TableRows/UserDataRow.jsx
@@ -1,4 +1,5 @@
 const UserDataRow = ({ user }) => {
+  console.log(user)
     return (
       <tr>
         <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
--- Client-Side/src/pages/Dashboard/Admin/ManageUsers/ManageUsers.jsx
+++ Client-Side/src/pages/Dashboard/Admin/ManageUsers/ManageUsers.jsx
@@ -1,5 +1,62 @@
+import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
+import { getUser } from "../../../../Utils/auth";
+import Loader from "../../../../components/Shared/Loader";
+import UserDataRow from "../../../../components/TableRows/UserDataRow";
+
 const ManageUsers = () => {
-  return <div>ManageUsers</div>;
+  const { data: users = [], isLoading } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => getUser(),
+  });
+  if(isLoading) return <Loader/>;
+  return (
+    <>
+      <div className="container mx-auto px-4 sm:px-8">
+        <Helmet>
+          <title>Manage Users</title>
+        </Helmet>
+        <div className="py-8">
+          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+              <table className="min-w-full leading-normal">
+                <thead>
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                    >
+                      Email
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                    >
+                      Role
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                    >
+                      Status
+                    </th>
+
+                    <th
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                    >
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>{/* User data table row */}{users?.map(user=><UserDataRow key={user?._id} user={user} />)}</tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ManageUsers;
