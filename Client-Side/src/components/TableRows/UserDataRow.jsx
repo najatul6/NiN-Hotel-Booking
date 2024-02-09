3def828200e742e5ec2f4eb729051786a9eca13a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-09 22:06:08 +0600

    okay

--- Client-Side/src/components/TableRows/UserDataRow.jsx
+++ Client-Side/src/components/TableRows/UserDataRow.jsx
@@ -3,10 +3,9 @@ import UpdateUserModal from "../Modal/UpdateUserModal";
 
 const UserDataRow = ({ user }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const modalHandler=()=>{
+  const modalHandler = () => {
     // Update user role
-
-  }
+  };
   return (
     <tr>
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -30,7 +29,10 @@ const UserDataRow = ({ user }) => {
       </td>
 
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        <span onClick={()=>setIsOpen(true)} className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+        <span
+          onClick={() => setIsOpen(true)}
+          className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
+        >
           <span
             aria-hidden="true"
             className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
@@ -38,7 +40,12 @@ const UserDataRow = ({ user }) => {
           <span className="relative">Update Role</span>
         </span>
         {/* Modal */}
-        <UpdateUserModal isOpen={isOpen} setIsEditModalOpen={setIsOpen} user={user} modalHandler={modalHandler}/>
+        <UpdateUserModal
+          isOpen={isOpen}
+          setIsEditModalOpen={setIsOpen}
+          user={user}
+          modalHandler={modalHandler}
+        />
       </td>
     </tr>
   );
