ddd7c1283e9c3c6d9dd96ffc325c4d1bc97c6f84
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-09 22:05:40 +0600

    send data to update user role modal

--- Client-Side/src/components/TableRows/UserDataRow.jsx
+++ Client-Side/src/components/TableRows/UserDataRow.jsx
@@ -1,38 +1,47 @@
+import { useState } from "react";
+import UpdateUserModal from "../Modal/UpdateUserModal";
+
 const UserDataRow = ({ user }) => {
-    return (
-      <tr>
-        <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-          <p className='text-gray-900 whitespace-no-wrap'>{user?.email}</p>
-        </td>
-        <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-          <p className='text-gray-900 whitespace-no-wrap'>{user?.role}</p>
-        </td>
-        <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-          {user?.status ? (
-            <p
-              className={`${
-                user.status === 'Verified' ? 'text-green-500' : 'text-yellow-500'
-              } whitespace-no-wrap`}
-            >
-              {user.status}
-            </p>
-          ) : (
-            <p className='text-red-500 whitespace-no-wrap'>Unavailable</p>
-          )}
-        </td>
-  
-        <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-          <span className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'>
-            <span
-              aria-hidden='true'
-              className='absolute inset-0 bg-green-200 opacity-50 rounded-full'
-            ></span>
-            <span className='relative'>Update Role</span>
-          </span>
-          {/* Modal */}
-        </td>
-      </tr>
-    )
+  const [isOpen, setIsOpen] = useState(false);
+  const modalHandler=()=>{
+    // Update user role
+
   }
-  
-  export default UserDataRow
+  return (
+    <tr>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">{user?.email}</p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">{user?.role}</p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        {user?.status ? (
+          <p
+            className={`${
+              user.status === "Verified" ? "text-green-500" : "text-yellow-500"
+            } whitespace-no-wrap`}
+          >
+            {user.status}
+          </p>
+        ) : (
+          <p className="text-red-500 whitespace-no-wrap">Unavailable</p>
+        )}
+      </td>
+
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <span onClick={()=>setIsOpen(true)} className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+          <span
+            aria-hidden="true"
+            className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+          ></span>
+          <span className="relative">Update Role</span>
+        </span>
+        {/* Modal */}
+        <UpdateUserModal isOpen={isOpen} setIsEditModalOpen={setIsOpen} user={user} modalHandler={modalHandler}/>
+      </td>
+    </tr>
+  );
+};
+
+export default UserDataRow;
