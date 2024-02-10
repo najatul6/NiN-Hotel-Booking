3827cb581b0f682777a2b19dc3175c06205470ea
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-10 23:59:55 +0600

    user and pass done

--- Server-Side/index.js
+++ Server-Side/index.js
@@ -39,7 +39,9 @@ const verifyToken = async (req, res, next) => {
 };
 
 // Send email
-
+const sendEmail=()=>{
+  
+}
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.DB_URI, {
