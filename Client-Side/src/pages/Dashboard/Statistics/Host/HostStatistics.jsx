2032ccff91e5d83cb91f0ce83ff8e7e47d87307f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-12 23:28:02 +0600

    graph chart design

--- Client-Side/src/pages/Dashboard/Statistics/Host/HostStatistics.jsx
+++ Client-Side/src/pages/Dashboard/Statistics/Host/HostStatistics.jsx
@@ -80,7 +80,7 @@ const HostStatistics = () => {
   
           <div className='mb-4 grid grid-cols-1 gap-6 lg:grid-cols-2 xl:grid-cols-3'>
             {/* Total Sales Graph */}
-            <div className=''>
+            <div className='relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md overflow-hidden xl:col-span-2'>
               <SalesLineChart />
             </div>
             {/* Calender */}
