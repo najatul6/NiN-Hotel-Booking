f7bda9691f1e24673c2a9251dfcfb81da5ccacf6
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-05-23 05:17:49 +0600

    link added

--- Client-Side/src/components/Shared/Footer/Footer.jsx
+++ Client-Side/src/components/Shared/Footer/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="px-4 divide-y text-gray-800 relative bottom-0 left-0">
       <div className="py-6 text-sm text-center text-gray-400 font-light">
-        &copy; 2024 HOTEL.NIN All rights reserved.
+        &copy; 2024 <a href="www.linkedin.com/in/najatul-islam-94193b1b3" target="_blank">HOTEL.NIN</a>  All rights reserved.
       </div>
     </footer>
   );
